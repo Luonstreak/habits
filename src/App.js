e6db0299e0503c9a270ea425247a3386726b1fa0
Lucio Rodriguez <luonstreak@gmail.com>
2019-07-31 00:47:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: {},
+      days: [...Array(32).keys()]
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state.habits);
+  }
+
+  turnActive = el => {
+    this.setState(prevState => ({
+      ...prevState,
+      code: { ...prevState.code, [el]: true }
+    }));
+  };
+
+  renderHeaders = () => {
+    return this.state.days.map(el => (
+      <td key={el} className="header-box">
+        <h1>{el}</h1>
+      </td>
+    ));
+  };
+
+  renderBoxes = () => {
+    const { code } = this.state;
+    return this.state.days.map(el => (
+      <td
+        key={el}
+        className={code[el] ? "box active" : "box"}
+        onClick={() => this.turnActive(el)}
+      />
+    ));
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="header">
+          <h2>HABIT TRACKER</h2>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <td className="title">HABIT</td>
+              {this.renderHeaders()}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="title">CODE</td>
+              {this.renderBoxes()}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
